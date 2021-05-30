63780c40543ff8965d222db630ed5a1531db911b
drewhgood <drewhgood@gmail.com>
2021-05-30 13:22:24 -0700

    test SSR

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,36 +1,48 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
-import {  InferGetStaticPropsType } from 'next'
+import {  InferGetServerSidePropsType } from 'next'
 import {prisma} from '../../lib/prisma'
 
-export default function Home({post}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        {
-          post && (
-            <>
-            <h1 className={styles.title}>Post: {post.id}</h1>
-            <h3 className={styles.title}>Post: {post.title}</h3>
-            </>
-          )
-}
-      </main>
-    </div>
-  );
-}
+export default function Home({ post }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+            if (!post) return null
+                 return (
+                   <div className={styles.container}>
+                     <Head>
+                       <title>Create Next App</title>
+                       <link rel="icon" href="/favicon.ico" />
+                     </Head>
 
-export const getStaticProps = async ({params}) => {
+                     <main className={styles.main}>
+                       {post && (
+                         <>
+                           <h1 className={styles.title}>
+                             Post:{' '}
+                             {
+                               //@ts-ignore
+                               post.id
+                             }
+                           </h1>
+                           <h3 className={styles.title}>
+                             Post:{' '}
+                             {
+                               //@ts-ignore
+                               post.title
+                             }
+                           </h3>
+                         </>
+                       )}
+                     </main>
+                   </div>
+                 );
+               }
+
+export const getServerSideProps = async ({params}) => {
   const post = await prisma.post.findUnique({ where: { id: Number(params.id) } });
   return { props: { post } }
 }
 
-export async function getStaticPaths() {
+export async function getServerSidePaths() {
   const allPosts = await prisma.post.findMany() || [];
 
   return {
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,36 +1,38 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
-import { InferGetStaticPropsType } from 'next';
+import { InferGetServerSidePropsType } from 'next';
 import {prisma} from '../../lib/prisma';
 import Link from 'next/link'
 
-export default function Home({ postIds }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({ postIds }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+                 return (
+                   <div className={styles.container}>
+                     <Head>
+                       <title>Create Next App</title>
+                       <link rel="icon" href="/favicon.ico" />
+                     </Head>
 
-      <main className={styles.main}>
-        <h1>All Posts</h1>
-        <ul>
-          {postIds.map(id => {
-            return (
-              <li>
-                <Link href={`/posts/${id}`}>
-                  <a>{`Post: ${id}`}</a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </main>
-    </div>
-  );
-}
+                     <main className={styles.main}>
+                       <h1>All Posts</h1>
+                       <ul>
+                         {
+                           //@ts-ignore
+                         postIds.map((id) => {
+                           return (
+                             <li>
+                               <Link href={`/posts/${id}`}>
+                                 <a>{`Post: ${id}`}</a>
+                               </Link>
+                             </li>
+                           );
+                         })}
+                       </ul>
+                     </main>
+                   </div>
+                 );
+               }
 
-export const getStaticProps = async ({ params }) => {
+export const getServerSideProps = async ({ params }) => {
   const posts = await prisma.post.findMany() || [];
   const postIds = posts.map(({id}) => id)
   return { props: { postIds } };
