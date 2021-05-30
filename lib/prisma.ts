062205f98d3a449527d7930d5403eaab32f0586b
drewhgood <drewhgood@gmail.com>
2021-05-30 12:52:55 -0700

    retry db connection fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 declare global {
   namespace NodeJS {
@@ -8,13 +8,18 @@ declare global {
   }
 }
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
+if (process.env.NODE_ENV === 'production') {
+  if (!prisma) {
+    prisma = new PrismaClient();
+  }
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
 
-if (!global.prisma) {
-  global.prisma = new PrismaClient()
+  prisma = global.prisma;
 }
-prisma = global.prisma
 
-
-export default prisma
+export { prisma };
