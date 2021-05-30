0693f8a6cd30b53066914fb85f04534894fa18bd
drewhgood <drewhgood@gmail.com>
2021-05-30 12:48:59 -0700

    fix prisma db connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -10,13 +10,11 @@ declare global {
 
 let prisma: PrismaClient
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient()
 }
+prisma = global.prisma
+
 
 export default prisma
